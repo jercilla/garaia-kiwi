0da07cc3e52ccc4b9cc101d9d3316ceb87a158a2
jercilla <joseba.ercilla@gmail.com>
2022-10-07 10:41:50 +0200

    FIX initial_data

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -1,19 +1,18 @@
+import sys
+
+sys.path.insert(0, "")
 from datetime import date
+from src.domain.barcode import BarCode, BarCodeRepository
 
 
 def main():
-    import sys
-
-    sys.path.insert(0, "")
-
-    from domain.barcode import BarCode, BarCodeRepository
 
     database_path = "data/database.db"
 
     barcode_repository = BarCodeRepository(database_path)
 
-    first_palot = BarCode(lote_number="K2100017001", project="L.CALIBRADO", date="")
-    barcode_repository.save_palot(first_palot)
+    # first_palot = BarCode(lote_number="K2100017001", project="L.CALIBRADO", date="")
+    # barcode_repository.save_palot(first_palot)
 
 
 if __name__ == "__main__":
